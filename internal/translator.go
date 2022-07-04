dd9e17e9c5390bcdfbaa82fc887048103d0a2e58
studokim <studokim@gmail.com>
2022-07-05 01:24:59 +0300

    Translator: move to translate-shell

--- internal/translator.go
+++ internal/translator.go
@@ -1,50 +1,31 @@
 package internal
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
 )
 
 type translator struct {
-	url        string
-	sourceLang string
 	targetLang string
 }
 
-type apiRequest struct {
-	Q       string `json:"q"`
-	Source  string `json:"source"`
-	Target  string `json:"target"`
-	Format  string `json:"format"`
-	Api_key string `json:"api_key"`
-}
-
-type apiResponse struct {
-	TranslatedText string `json:"translatedText"`
-}
-
 func newTranslator() translator {
-	return translator{url: "https://libretranslate.de/translate", sourceLang: "de", targetLang: "en"}
+	return translator{targetLang: "en"}
 }
 
 func (t *translator) translate(text string) (string, error) {
-	apiRequest := apiRequest{Q: text, Source: t.sourceLang, Target: t.targetLang, Format: "text", Api_key: ""}
-	requestBytes, err := json.Marshal(apiRequest)
+	workdir, err := os.Getwd()
 	if err != nil {
 		return "", err
 	}
-	response, err := http.Post(t.url, "application/json", bytes.NewBuffer(requestBytes))
+	transPath := path.Join(workdir, "/translator/trans")
+	command := exec.Command(transPath, "-no-warn", "-b", fmt.Sprintf(":%s", t.targetLang), text)
+	out, err := command.Output()
 	if err != nil {
 		return "", err
 	}
-	defer response.Body.Close()
-	responseBytes, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return "", err
-	}
-	apiResponse := apiResponse{}
-	json.Unmarshal(responseBytes, &apiResponse)
-	return apiResponse.TranslatedText, nil
+	return strings.TrimSpace(string(out)), nil
 }
